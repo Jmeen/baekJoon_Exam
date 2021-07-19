034ee539bab5e3dde375462c5a7b4d151b4f0f9b
BIT <BIT@DESKTOP-687L5VB>
2021-07-19 18:24:44 +0900

    Initial Commit - 백준 1단계 입출력 및 사칙연산 fin

---
+++ BAEKJOON_Solve/src/com/level1/Lv1_2.java
@@ -0,0 +1,79 @@
+package com.level1;
+import java.util.Scanner;
+
+public class Lv1_2 {
+	public static void main(String[] args)
+	
+	{
+		finEx();
+	
+	}
+	
+	private static void finEx() {
+		
+		//레벨 1-2 Q 10718 - 
+//		System.out.println("강한친구 대한육군\n강한친구 대한육군");
+		
+		//레벨 1-3 아래 예제와 같이 고양이를 출력하시오.
+		
+//		System.out.println("\\    /\\");
+//		System.out.println(" )  ( ')");
+//		System.out.println("(  /  )");
+//		System.out.println(" \\(__)|");
+
+		// 레벨 1-4 개를 출력한다.
+		
+//		System.out.println("|\\_/|");
+//		System.out.println("|q p|   /}");
+//		System.out.println("( 0 )\"\"\"\\");
+//		System.out.println("|\"^\"`    |");
+//		System.out.println("||_/=\\\\__|");
+		
+		
+		// 레벨 1-5 Q1000 A+B / 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+		
+//		Scanner _sc = new Scanner(System.in);
+//		int a = _sc.nextInt();
+//		int b = _sc.nextInt();
+//		System.out.println(a+b);
+		
+		// 레벨 1-6~8
+//		Scanner sc = new Scanner(System.in);
+//		int a = sc.nextInt();
+//		int b = sc.nextInt();
+//		System.out.println(a+b);
+//		System.out.println(a-b);	
+//		System.out.println(a*b);	
+//		System.out.println(a/b);		
+//		System.out.println(a%b);	
+		
+		// 레벨 1-9
+//		Scanner sc = new Scanner(System.in);
+//		int A = sc.nextInt();
+//		int B = sc.nextInt();
+//		int C = sc.nextInt();
+//		System.out.println((A+B)%C);
+//		System.out.println(((A%C) + (B%C))%C);	
+//		System.out.println((A*B)%C);
+//		System.out.println(((A%C) * (B%C))%C);		
+		
+		
+		// 레벨 1 - 10
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		int b_1 = Character.getNumericValue(Integer.toString(B).charAt(0));
+		int b_2 = Character.getNumericValue(Integer.toString(B).charAt(1));	
+		int b_3 = Character.getNumericValue(Integer.toString(B).charAt(2));
+
+		System.out.println(A*b_3);	
+		System.out.println(A*b_2);		
+		System.out.println(A*b_1);	
+		System.out.println(A*B);	
+	
+	}
+}
+
+
+
